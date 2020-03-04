ccc02498e97cf657b392dc78e22397427e861308
Juan Delgado <jcdelgadop76@gmail.com>
2020-03-04 12:12:45 -0500

    Initial Import

---
+++ README.md
@@ -0,0 +1,13 @@
+﻿## Código fuente Desafio Platzi en Server eXpress!
+
+Este es el ejercicio del curso, Grid LayOut, desafio Platzi Video, 100% Responsive.
+
+
+Recuerden instalar los paquetes de node
+
+...
+
+npm install
+...
+
+JCD
---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require("path");
+const config = require('./server/config/config');
+const frameguard = require('frameguard');
+const app = express();
+
+app.use(frameguard({ action: 'deny' }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function(req, res) {
+    //console.log(`__dirname: ${__dirname}`);
+
+    res.sendFile(__dirname + '/public/');
+
+});
+
+app.listen(process.env.PORT, () => {
+    console.log(`Escuchando por el puerto ${process.env.PORT}: `);
+});
